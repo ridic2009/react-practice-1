a090f8e00b70c5f6a768f340045125ebce563ade
daniels <daniel.shchepin@gmail.com>
2023-06-19 01:44:23 +0700

    2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 function App() {
   return (
     <div className="page">
+
+      <div className="overlay">
+        <div className="sidepage">
+          <h1>123</h1>
+        </div>
+      </div>
+
       <header>
         <div className="container">
           <div className="header__container">
@@ -82,9 +89,33 @@ function App() {
       <main className="content">
         <section className="products">
           <div className="container">
-            <h1>Все кроссовки</h1>
+            <div className="products__top">
+              <h1>Все кроссовки</h1>
+              <div className="search">
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M15.25 15.25L11.8855 11.8795L15.25 15.25ZM13.75 7.375C13.75 9.06576 13.0784 10.6873 11.8828 11.8828C10.6873 13.0784 9.06576 13.75 7.375 13.75C5.68424 13.75 4.06274 13.0784 2.86719 11.8828C1.67165 10.6873 1 9.06576 1 7.375C1 5.68424 1.67165 4.06274 2.86719 2.86719C4.06274 1.67165 5.68424 1 7.375 1C9.06576 1 10.6873 1.67165 11.8828 2.86719C13.0784 4.06274 13.75 5.68424 13.75 7.375V7.375Z"
+                    stroke="#E4E4E4"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                  />
+                </svg>
+                <input
+                  type="search"
+                  name="search"
+                  id="search"
+                  placeholder="Поиск..."
+                />
+              </div>
+            </div>
 
-            <ul>
+            <ul className="products-list">
               <li className="card">
                 <img
                   width={133}
@@ -96,7 +127,85 @@ function App() {
                 <div className="card__inner">
                   <div>
                     <span>Цена:</span>
-                    <b>12 999 ₽</b>
+                    <strong>12 999 ₽</strong>
+                  </div>
+                  <button>
+                    <svg
+                      width="32"
+                      height="32"
+                      viewBox="0 0 32 32"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect
+                        x="0.5"
+                        y="0.5"
+                        width="31"
+                        height="31"
+                        rx="7.5"
+                        fill="white"
+                        stroke="#F2F2F2"
+                      />
+                      <path
+                        d="M20.6653 15.1312H17.2021V11.6682C17.2021 10.3328 15.1311 10.3328 15.1311 11.6682V15.1312H11.668C10.3329 15.1312 10.3329 17.2022 11.668 17.2022H15.1311V20.6652C15.1311 22.0005 17.2021 22.0005 17.2021 20.6652V17.2022H20.6653C22.0005 17.2022 22.0005 15.1312 20.6653 15.1312Z"
+                        fill="#D3D3D3"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </li>
+
+              <li className="card">
+                <img
+                  width={133}
+                  height={112}
+                  src="/assets/card-img-1.jpg"
+                  alt="card-image"
+                />
+                <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
+                <div className="card__inner">
+                  <div>
+                    <span>Цена:</span>
+                    <strong>12 999 ₽</strong>
+                  </div>
+                  <button>
+                    <svg
+                      width="32"
+                      height="32"
+                      viewBox="0 0 32 32"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect
+                        x="0.5"
+                        y="0.5"
+                        width="31"
+                        height="31"
+                        rx="7.5"
+                        fill="white"
+                        stroke="#F2F2F2"
+                      />
+                      <path
+                        d="M20.6653 15.1312H17.2021V11.6682C17.2021 10.3328 15.1311 10.3328 15.1311 11.6682V15.1312H11.668C10.3329 15.1312 10.3329 17.2022 11.668 17.2022H15.1311V20.6652C15.1311 22.0005 17.2021 22.0005 17.2021 20.6652V17.2022H20.6653C22.0005 17.2022 22.0005 15.1312 20.6653 15.1312Z"
+                        fill="#D3D3D3"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </li>
+
+              <li className="card">
+                <img
+                  width={133}
+                  height={112}
+                  src="/assets/card-img-1.jpg"
+                  alt="card-image"
+                />
+                <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
+                <div className="card__inner">
+                  <div>
+                    <span>Цена:</span>
+                    <strong>12 999 ₽</strong>
                   </div>
                   <button>
                     <svg
